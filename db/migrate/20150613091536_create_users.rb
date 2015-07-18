06767bbfb7b3e9d17c30d45206ff3772216b46b4
Yumiko Huang <dayumikoda@gmail.com>
2015-07-17 23:18:24 -0700

    basic setup

--- db/migrate/20150613091536_create_users.rb
+++ db/migrate/20150613091536_create_users.rb
@@ -4,12 +4,11 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :account_type
-      t.integer :cashback
+      t.float :cashback
       t.integer :click
       t.integer :feed
       t.string :link
-      t.string :description, :default => 'I love Fashion and Cashback'
-
+      t.string :description, :default => 'We love Fashion.'
 
       t.timestamps null: false
     end
---
+++ db/schema.rb
@@ -0,0 +1,61 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150714053256) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "amznasins", force: :cascade do |t|
+    t.string   "asin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photofeeds", force: :cascade do |t|
+    t.string   "asin"
+    t.string   "description"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
+    t.integer  "user_id"
+    t.integer  "amznasin_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "photofeeds", ["amznasin_id"], name: "index_photofeeds_on_amznasin_id", using: :btree
+  add_index "photofeeds", ["user_id"], name: "index_photofeeds_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "account_type"
+    t.float    "cashback"
+    t.integer  "click"
+    t.integer  "feed"
+    t.string   "link"
+    t.string   "description",     default: "We love Fashion."
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.string   "amzn_email"
+    t.string   "real_name"
+    t.string   "oauth_token"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+end
